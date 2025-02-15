34bb70cf486c91b003554e666f087d766eee292a
lemmoor <49235295+lemmoor@users.noreply.github.com>
2025-02-15 12:36:47 +0100

    WIP - generating study plans

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from camel.models import ModelFactory
 from camel.types import ModelPlatformType, ModelType
 from camel.configs import AIMLConfig
 from camel.agents import ChatAgent
+from camel.societies.workforce import Workforce
+from camel.tasks import Task
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,18 +19,51 @@ model = ModelFactory.create(
     model_config_dict=AIMLConfig().as_dict(),
 )
 
-# Define an assitant message
-system_msg = "You are a helpful assistant."
+# Initialize the agents
+planner_agent = ChatAgent(
+    "You are a curriculum planner specialising in effective study plans", model=model
+)
+time_manager_agent = ChatAgent("You are a time management advisor", model=model)
+professor_agent = ChatAgent("You are a professor", model=model)
 
-# Initialize the agent
-agent = ChatAgent(system_msg, model=model)
+# Create a workforce instance
+workforce = Workforce(
+    "A Simple Workforce focused on education",
+    coordinator_agent_kwargs={"model": model},
+    task_agent_kwargs={"model": model},
+    new_worker_agent_kwargs={"model": model},
+)
+
+workforce.add_single_agent_worker(
+    "A curriculum planner specialising in effective study plans",
+    worker=planner_agent,
+).add_single_agent_worker(
+    "A time management advisor",
+    worker=time_manager_agent,
+).add_single_agent_worker(
+    "A professor agent",
+    worker=professor_agent,
+)
+
+task = Task(
+    content="Make a study plan for calculus 1",
+    id="0",
+)
+task = workforce.process_task(task)
+print(task)
 
 
 @app.route("/")
 def home():
-    response = agent.step("Hello, can you help me?")
-    print(response.msgs)
-    return response.msgs[0].content
+    # response = agent.step("Hello, can you help me?")
+    # return response.msgs[0].content
+    # task = Task(
+    #     content="Make a study plan for calculus 1",
+    #     id="0",
+    # )
+    # task = workforce.process_task(task)
+    # print(task)
+    return "???"
 
 
 if __name__ == "__main__":
